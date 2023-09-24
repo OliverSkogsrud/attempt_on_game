43248b996340320b7d6fa8ed121afac20c9d8c3b
oliver <oliverprogaming23@gmail.com>
2023-09-24 11:34:40 +0200

    sdaw

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ bullet = pygame.image.load("bullet_pixel.png")
 small_bullet = pygame.transform.scale(bullet, (50,50))
 bullet_rect = small_bullet.get_rect(topleft = (player_x, bullet_y))
 bullet_copy = small_bullet.copy()
+bullet_copy_rect = bullet_copy.get_rect(topleft = (player_x, 400))
 
 running = True
 
@@ -36,20 +37,25 @@ while running:
             pygame.quit()
             exit()
         #Movement
-        #if event.type == pygame.KEYDOWN:
-            #if event.key == pygame.K_RIGHT:
-                #player_x += 25
-            #if event.key == pygame.K_LEFT:
-                #player_x -= 25
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                
+                bullet_rect.y -= 200
+                if bullet_rect.y <= -10 and event.key == pygame.K_SPACE: 
+                    
+                    bullet_copy_rect.y -= 100
+                
+             
             
     
     
     #movement and bullet
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        screen.blit(small_bullet, (player_x, bullet_y))
-        bullet_y -= 100
-        screen.blit(bullet_copy, (player_x, bullet_y))
+    #if keys[pygame.K_SPACE]:
+        #screen.blit(small_bullet, (player_x, bullet_y))
+        #bullet_y -= 100
+        #screen.blit(bullet_copy, (player_x, bullet_y))
         
    
         
@@ -63,7 +69,8 @@ while running:
 
         
         
-    
+    screen.blit(small_bullet, bullet_rect)
+    screen.blit(bullet_copy, bullet_copy_rect)
     screen.blit(small_player, (player_x ,400))
     
     pygame.display.flip()
