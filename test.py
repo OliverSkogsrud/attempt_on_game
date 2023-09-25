0c5c5aca78e6a5a10d33645f0680f079130a26ce
oliver <oliverprogaming23@gmail.com>
2023-09-25 20:59:23 +0200

    Enemies added

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pygame
 import math
+import time
+import threading
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -13,7 +15,9 @@ transparent = (0, 0, 0, 0)
 #variables
 player_x = 300
 
-  
+
+
+
 
 test_font = pygame.font.Font(None, 50)#hvis jeg skal ha en font må jeg laste den ned og legge den til
 #images
@@ -38,12 +42,39 @@ class Bullet:
         self.rect.x += self.dx
         self.rect.y += self.dy
 
-    def draw(self, screen):
+    def draw(self):
         screen.blit(self.image, self.rect)
 
 bullets = []
 
-        
+class Enemy:
+    def __init__(self, x, y):
+        self.image = pygame.Surface((50,50))     
+        self.image.fill((0,255,0))   
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.speed = 3
+        self.angle = 300
+        self.dx = self.speed * math.cos(self.angle) 
+        self.dy = -self.speed * math.sin(self.angle)
+    
+    def update(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+    
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+enemies = []
+
+def spawnenemy():
+    threading.Timer(1.0, spawnenemy).start()
+    enemy = Enemy(400,0)
+    enemies.append(enemy)
+
+
+spawnenemy()
+
 
 running = True
 
@@ -57,11 +88,21 @@ while running:
                 bullet = Bullet(player_x, 400)
                 bullets.append(bullet)
     
+    
+
+    #enemy = Enemy(400,0)
+    #enemies.append(enemy)
+
     screen.fill((0,75,220))
 
     for bullet in bullets:
         bullet.update()
-        bullet.draw(screen)
+        bullet.draw()
+
+    for enemy in enemies:
+        enemy.update()
+        enemy.draw()   
+
     
     #movement and bullet
    
