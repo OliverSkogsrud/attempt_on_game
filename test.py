b43aa3e42b6393f6fab0004484056f7fbc33ee8f
oliver <oliverprogaming23@gmail.com>
2023-09-25 20:25:17 +0200

    shooting works

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pygame
-from sys import exit
+import math
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -12,7 +12,7 @@ clock = pygame.time.Clock()
 transparent = (0, 0, 0, 0)
 #variables
 player_x = 300
-bullet_y = 400
+
   
 
 test_font = pygame.font.Font(None, 50)#hvis jeg skal ha en font må jeg laste den ned og legge den til
@@ -22,65 +22,60 @@ Player = pygame.image.load("spaceship.jpg")
 small_player = pygame.transform.scale(Player, (100,100))
 player_rect = small_player.get_rect(topleft = (player_x,400))
 #bullet
-bullet = pygame.image.load("bullet_pixel.png")
-small_bullet = pygame.transform.scale(bullet, (50,50))
-bullet_rect = small_bullet.get_rect(topleft = (player_x, bullet_y))
-bullet_copy = small_bullet.copy()
-bullet_copy_rect = bullet_copy.get_rect(topleft = (player_x, 400))
+
+class Bullet:
+    def __init__(self,x,y):
+        self.image = pygame.Surface((10,20))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.speed = 5
+        self.angle = -300
+        self.dx = self.speed * math.cos(self.angle) 
+        self.dy = -self.speed * math.sin(self.angle)
+
+    def update(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+bullets = []
 
         
 
 running = True
 
 while running:
-    screen.fill((0,75,220))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        #Movement
-        
-        if event.type == pygame.KEYDOWN:
+            running = False
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                
-                bullet_rect.y -= 200
-                if bullet_rect.y <= -10 and event.key == pygame.K_SPACE: 
-                    
-                    bullet_copy_rect.y -= 100
-                
-             
-            
+
+                bullet = Bullet(player_x, 400)
+                bullets.append(bullet)
     
+    screen.fill((0,75,220))
+
+    for bullet in bullets:
+        bullet.update()
+        bullet.draw(screen)
     
     #movement and bullet
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        screen.blit(small_bullet, bullet_rect)
-        screen.blit(bullet_copy, bullet_copy_rect)
-        screen.blit(small_bullet, (player_x, bullet_y))
-        while True:
-            bullet_copy_rect.y += 10
-            if bullet_copy_rect.y <= -100:
-                bullet_copy_rect.y == 400
-                bullet_rect.x == player_x
-        
    
-        
-
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player_x -= 7
+        player_x -= 5
 
     if keys[pygame.K_RIGHT]:
-        player_x += 7
-            
-
-        
-        
+        player_x += 5
     
     screen.blit(small_player, (player_x ,400))
     
     pygame.display.flip()
 
-    pygame.display.update()
     clock.tick(60)
 
+pygame.quit()
